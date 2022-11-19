c436e7af3f8426e9fb9014b107e5310ac8cb8951
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2022-11-19 20:10:06 -0300

    added disabled state classes

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -7,13 +7,17 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
   className?: string
 }
 
-export function Button({ children, asChild, className }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
   const Comp = asChild ? Slot : "button"
 
   const clsxClass = clsx(
     className,
-    "text-sm w-full outline-none font-semibold px-4 py-3 rounded bg-cyan-500 transition-all hover:bg-cyan-300 focus:ring-2 ring-gray-100"
+    "text-sm w-full disabled:brightness-75 disabled:cursor-not-allowed outline-none font-semibold px-4 py-3 rounded bg-cyan-500 transition-all enabled:hover:bg-cyan-300 focus:ring-2 ring-gray-100"
   )
 
-  return <Comp className={clsxClass}>{children}</Comp>
+  return (
+    <Comp className={clsxClass} {...props}>
+      {children}
+    </Comp>
+  )
 }
